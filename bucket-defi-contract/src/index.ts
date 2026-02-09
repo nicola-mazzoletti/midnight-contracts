e5e95fdfd3e458811cdc461b6714ae098bf6a896
erick <erickrs2esuarez@gmail.com>
2026-02-09 08:33:01 -0300

    update

--- bucket-defi-contract/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * as BucketDefi from "./managed/bucket-defi/contract/index.js";
-export * from "./witnesses";
--- counter-contract/src/test/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-export const randomBytes = (length: number): Uint8Array => {
-  const bytes = new Uint8Array(length);
-  crypto.getRandomValues(bytes);
-  return bytes;
-};
-
-export const toHexPadded = (str: string, len = 64) =>
-  Buffer.from(str, "ascii").toString("hex").padStart(len, "0");
